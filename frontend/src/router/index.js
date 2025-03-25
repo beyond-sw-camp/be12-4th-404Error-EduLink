d4c263464169d66004fb4c2841a107f92921ceb6
kuj7882 <kuj7882@naver.com>
2025-03-25 10:11:25 +0900

    [Test] test파일 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -43,7 +43,7 @@ import question from '../pages/board/question.vue';
 
 
 import common from '../pages/commondashboard.vue';
-import test from '../pages/test.vue';
+import test from '../pages/test/Test.vue';
 
 
 const router = createRouter({
@@ -52,7 +52,6 @@ const router = createRouter({
         { path: '/a', component: FormsView },
         { path: '/', component: LoginForm, meta: { hideAside: true, hideNav: true } },
         { path: '/Signup', component: SignupForm, meta: { hideAside: true, hideNav: true } },
-        { path: '/test', component: testForm },
         {
             path: '/studentdashboard',
             component: Studentdashboard,
@@ -61,7 +60,6 @@ const router = createRouter({
         },
 
         { path: '/userinfo', component: UserinfoForm, meta: { requiresAuth: true }  },
-
         { path: '/inst/dashboard', component: InstructorDash, meta: { requiresAuth: true }  },
         { path: '/inst/studinform', component: InstStudInform, meta: { requiresAuth: true }  },
         { path: '/inst/studinform/:userIdx', component: InstStudInform, meta: { requiresAuth: true }  },
@@ -97,9 +95,7 @@ const router = createRouter({
 
         { path: '/common', component: common, meta: { requiresAuth: true }  },
         { path: '/bootcampInfo', component: BootcampInfo, meta: { asideComponent: StudentAside,requiresAuth: true }  },
-
-
-
+        { path: '/test', component: test, meta: { requiresAuth: true }},
         // { path: '/c', component: Create },
         // { path: '/d', component: Signup },
         // { path: '/e', component: Login },
